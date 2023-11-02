02b118d9fb1b2068b3e70b1aa9f5f3401cf1a6cf
rgb2hsl <ru.web.designer@gmail.com>
2023-11-02 20:29:54 +0400

    test fixes

--- tests/src/withdraw.test.js
+++ tests/src/withdraw.test.js
@@ -11,10 +11,10 @@ import {
 import { ethers } from "ethers";
 
 const contractAddr = "0x4e1224f513048e18e7a1883985b45dc0fe1d917e";
-const pluginName = "P2P.org";
+const pluginFolder = "p2porg";
 const testNetwork = "ethereum";
 const abi_path =
-  `../networks/${testNetwork}/${pluginName}/abis/` + contractAddr + ".json";
+  `../networks/${testNetwork}/${pluginFolder}/abis/` + contractAddr + ".json";
 const abi = require(abi_path);
 
 MODELS.forEach((model) => {
